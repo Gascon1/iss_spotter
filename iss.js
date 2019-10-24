55cfacd1bb44be1f22c3a25f1f3170f1a01ffa38
Mikel Gascon <mikelgascon@outlook.com>
2019-10-24 23:00:48 +0000

    finalized the ISS tracker and made it readable for humans

--- iss.js
+++ iss.js
@@ -8,6 +8,7 @@ const request = require('request');
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
+
 const fetchMyIP = (callback) => {
   const URL = 'https://api.ipify.org?format=json'
   request(URL, (error, response, body) => {
@@ -77,6 +78,27 @@ const fetchISSFlyOverTimes = function (coords, callback) {
   });
 }
 
+const nextISSTimesForMyLocation = (callback) => {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+module.exports = { nextISSTimesForMyLocation };
